27b62cb8d1d176520e28c88ce9691bdeedf84e0f
Tom <tom.slaytor@gmail.com>
2022-09-27 15:27:02 +0300

    made the currency page more interactive, can now choose currencies

--- practice4cs50W/currency.js
+++ practice4cs50W/currency.js
@@ -9,16 +9,29 @@ var requestOptions = {
 
 document.addEventListener('DOMContentLoaded', function() {
     
-    // Send a GET request to the URL
-    fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=GBP,EUR&base=USD", requestOptions)
-    // Put response into json form
-    .then(response => response.json())
-    .then(data => {
-        // Log data to the console
-        const rate = data.rates.GBP
+  document.querySelector('form').onsubmit = function(){
+    const base = document.querySelector('#base').value.toUpperCase()
+    const symbol = document.querySelector('#symbol').value.toUpperCase()
 
-        document.querySelector('body').innerHTML = `1 USD is worth ${rate.toFixed(3)} GBP`
-    });
+    document.querySelector('#base').value = ''
+    document.querySelector('#symbol').value = ''
+
+      // Send a GET request to the URL
+      fetch(`https://api.apilayer.com/exchangerates_data/latest?symbols=${symbol}&base=${base}`, requestOptions)
+      // Put response into json form
+      .then(response => response.json())
+      .then(data => {
+          // Log data to the console
+          const rate = data.rates[`${symbol}`]
+  
+          document.querySelector('.result').innerHTML = `1 ${base} is worth ${rate.toFixed(3)} ${symbol}`
+      });
+
+    return false
+
+   
+  }
+  
 });
 
 
